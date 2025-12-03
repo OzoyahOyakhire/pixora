eaf8687e9d4785402606c998522a939a659a5f17
Justice Oyakhire <justiceoyakhire27@gmail.com>
2025-12-03 15:40:08 +0100

    added google login button and provider

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -11,9 +11,108 @@ export const schemaTypes = [
       },
       {
         name: 'image',
-        title: 'UserName',
+        title: 'Image',
         type: 'string',
       },
     ],
+  },
+  {
+    name: 'pin',
+    title: 'Pin',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'about',
+        title: 'About',
+        type: 'string',
+      },
+      {
+        name: 'destination',
+        title: 'Destination',
+        type: 'url',
+      },
+      {
+        name: 'category',
+        title: 'Category',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'userId',
+        title: 'UserId',
+        type: 'string',
+      },
+      {
+        name: 'postedBy',
+        title: 'PostedBy',
+        type: 'postedBy',
+      },
+      {
+        name: 'save',
+        title: 'Save',
+        type: 'array',
+        of: [{type: 'save'}],
+      },
+
+      {
+        name: 'comments',
+        title: 'Comments',
+        type: 'array',
+        of: [{type: 'comments'}],
+      },
+    ],
+  },
+  {
+    name: 'comments',
+    title: 'Comments',
+    type: 'document',
+    fields: [
+      {
+        name: 'postedBy',
+        title: 'PostedBy',
+        type: 'postedBy',
+      },
+      {
+        name: 'comment',
+        title: 'Comment',
+        type: 'string',
+      },
+    ],
+  },
+  {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+      {
+        name: 'postedBy',
+        title: 'PostedBy',
+        type: 'postedBy',
+      },
+      {
+        name: 'userId',
+        title: 'UserId',
+        type: 'string',
+      },
+    ],
+  },
+
+  {
+    name: 'postedBy',
+    title: 'PostedBy',
+    type: 'reference',
+    to: [{type: 'user'}],
   },
 ]
